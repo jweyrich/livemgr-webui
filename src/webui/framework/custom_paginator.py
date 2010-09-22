bc35595dbca17f69d993f8b76a69c53825ce7007
Jardel Weyrich <jweyrich@gmail.com>
2010-09-22 07:51:18 -0300

    Better method naming for CustomPaginator.

--- src/webui/framework/custom_paginator.py
+++ src/webui/framework/custom_paginator.py
@@ -10,22 +10,22 @@ class CustomPaginator(Paginator):
 	def __init__(self, request, queryset, per_page=25, page_number=None):
 		Paginator.__init__(self, queryset, per_page or 10)
 		if page_number == None:
-			page_number = self._page_from_request(request)
+			page_number = self._get_page_from_request(request)
 		self.page = self._paginate(page_number)
 	def _paginate(self, page_number):
 		try:
 			result = self.page(page_number)
 		except (EmptyPage, InvalidPage):
 			result = self.page(self.num_pages)
-		self._limited_page_range(page_number, 5)
+		self._limit_page_range(page_number, 5)
 		return result
-	def _page_from_request(self, request):
+	def _get_page_from_request(self, request):
 		try:
 			page = int(request.GET.get('page', '1'))
 		except ValueError:
 			page = 1
 		return page
-	def _limited_page_range(self, page_number, range_gap=5):
+	def _limit_page_range(self, page_number, range_gap=5):
 		start = page_number - range_gap
 		if start < range_gap:
 			start = 1
