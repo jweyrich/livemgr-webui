d82a4a6d2b94801b5d796ab0ed0a4d80eb3e16a3
Jardel Weyrich <jweyrich@gmail.com>
2014-03-29 05:47:12 -0300

    Show how to setup it for local testing.

---
+++ webui/settings_example.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2010 Jardel Weyrich
+#
+# This file is part of livemgr-webui.
+#
+# livemgr-webui is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# livemgr-webui is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with livemgr-webui. If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors:
+#   Jardel Weyrich <jweyrich@gmail.com>
+
+# Django settings for webui project.
+from settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'livemgr',
+        'USER': 'livemgr',
+        'PASSWORD': 'livemgr',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+MIDDLEWARE_CLASSES += (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
+INSTALLED_APPS += (
+    'debug_toolbar',
+)
+
+INTERNAL_IPS += ('<internal_ip_here>', '<another_here>', '<and_another_if_you_like>',)
+
+LICENSE_FILE = os.path.join(ROOT, os.pardir, 'conf', 'certs', 'cert.pem')
+KEYSERVER_HOST = '127.0.0.1' # No need to change this.
+
+########################
+# DJANGO DEBUG TOOLBAR #
+########################
+
+#DEBUG_TOOLBAR_PANELS = (
+#    'debug_toolbar.panels.version.VersionDebugPanel',
+#    'debug_toolbar.panels.timer.TimerDebugPanel',
+#    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+#    'debug_toolbar.panels.headers.HeaderDebugPanel',
+#    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+#    'debug_toolbar.panels.template.TemplateDebugPanel',
+#    'debug_toolbar.panels.sql.SQLDebugPanel',
+#    'debug_toolbar.panels.signals.SignalDebugPanel',
+#    'debug_toolbar.panels.logger.LoggingPanel',
+#)
+
+def show_toolbar(request):
+    user = request.user
+    if hasattr(user, 'get_profile'):
+        return user.get_profile().debug
+    return False
+
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False,
+    'SHOW_TOOLBAR_CALLBACK': show_toolbar,
+    'HIDE_DJANGO_SQL': False,
+}
